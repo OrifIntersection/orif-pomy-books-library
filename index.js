95a4439fda3ea9531f2c0e1b15ef156a46b17495
ljhaesler <James.Haesler@formation.orif.ch>
2025-10-21 14:54:05 +0000

    Basic JSON Testing

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+const fs = require('fs')
+
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Test')
+const booksData = fs.readFileSync(`${__dirname}/dev-data/data/books.json`, 'utf-8')
+const books = JSON.parse(booksData)
+
+app.get('/api', (req, res) => {
+  res.json(books)
 })
 
 app.listen(port, () => {
