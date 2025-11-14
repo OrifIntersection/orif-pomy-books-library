db0540cf13503a058258ff7453a937ccb2b5c0d5
ljhaesler <James.Haesler@formation.orif.ch>
2025-11-14 10:54:26 +0000

    Simple Vite front-end

--- orif-pomy-books-library-front-end/src/App.jsx
+++ orif-pomy-books-library-front-end/src/App.jsx
@@ -1,34 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import tempBooksData from './temp.json'
+
+function BookTable({ books }) {
+
+  // add <tr key={book._id}>
+  return books.map(book =>
+    <tr>
+      <td>{book.Title}</td>
+      <td>{book.Author.join(", ")}</td>
+      <td>{book.Genre.join(", ")}</td>
+      <td>{book.Subject.join(", ")}</td>
+      <td>{book.Location}</td>
+      <td>{book.ISBN}</td>
+    </tr>
+  )
+}
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [books, setBooks] = useState([])
+
+  useEffect(() => {
+
+    async function getAPI() {
+      try {
+        const res = await fetch("https://orif-pomy-books-library.vercel.app/api/v1/books");
+        if (res.ok) {
+
+          const resBooks = await res.json()
+          console.log(resBooks);
+          setBooks(resBooks)
+        }
+      } catch (error) {
+        setBooks(tempBooksData);
+        console.log(error)
+      }
+    }
+    getAPI();
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <table>
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Author</th>
+          <th>Genre</th>
+          <th>Subject</th>
+          <th>Location</th>
+          <th>ISBN</th>
+        </tr>
+
+      </thead>
+      <tbody>
+        <BookTable books={books} />
+      </tbody>
+    </table>
   )
 }
 
