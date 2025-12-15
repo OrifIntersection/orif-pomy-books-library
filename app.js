4d1723f4626b432afab6854a5e608984a921de07
ljhaesler <James.Haesler@formation.orif.ch>
2025-12-15 09:27:21 +0000

    Check request headers

--- app.js
+++ app.js
@@ -13,7 +13,10 @@ const app = express();
 app.use(cors({ origin: "https://ideal-trout-5g7x9xxvxg462p-5173.app.github.dev" }));
 app.use(express.json());
 
-console.log(req.headers);
+app.use((req, res, next) => {
+  console.log(req.headers);
+  next();
+});
 
 // routers
 app.use("/api/v1/books", booksRouter);
