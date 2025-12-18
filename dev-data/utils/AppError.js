3bc47aa9c5358d72fe45931546af2a12161ad6dd
ljhaesler <James.Haesler@formation.orif.ch>
2025-12-18 09:19:50 +0000

    bugfix

--- dev-data/utils/AppError.js
+++ dev-data/utils/AppError.js
@@ -17,7 +17,7 @@ class AppError extends Error {
 
     super(errorDef.message);
     this.statusCode = errorDef.statusCode;
-    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+    this.status = `${this.statusCode}`.startsWith('4') ? 'fail' : 'error';
     this.isOperational = true;
     Error.captureStackTrace(this, this.constructor);
   }
