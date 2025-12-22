c4418fc1d9f9c6cb74821f97b220e96219046b3c
ljhaesler <James.Haesler@formation.orif.ch>
2025-12-22 07:37:48 +0000

    only return undeleted distinct fields

--- dev-data/handlers/booksHandler.js
+++ dev-data/handlers/booksHandler.js
@@ -203,9 +203,9 @@ export async function getDistinctFields(req, res, next) {
 
   // will return each distinct/unique field on /api/v1/books/distinct
 
-  const uniqueGenres = await Book.distinct("Genre")
-  const uniqueSubjects = await Book.distinct("Subject");
-  const uniqueLocations = await Book.distinct("Location");
+  const uniqueGenres = await Book.distinct("Genre", { Deleted: false })
+  const uniqueSubjects = await Book.distinct("Subject", { Deleted: false });
+  const uniqueLocations = await Book.distinct("Location", { Deleted: false });
 
   const resData = { Genres: uniqueGenres, Subjects: uniqueSubjects, Locations: uniqueLocations };
 
