88e3bdc3aeb477f9bcc19c16da872d9e5d22b3e8
ljhaesler <lithiumium@gmail.com>
2026-01-15 08:33:22 +0100

    Use port 587 for emails

--- app.js
+++ app.js
@@ -78,8 +78,8 @@ app.use((err, req, res, next) => {
 
 const transporter = nodemailer.createTransport({
   host: "smtp.resend.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: "resend",
     pass: process.env.RESEND_API_KEY,
