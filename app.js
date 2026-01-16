6bb5c2e8a56d270c0b63da8c36f8273f2f5e2360
ljhaesler <lithiumium@gmail.com>
2026-01-16 10:05:23 +0100

    bugfix

--- app.js
+++ app.js
@@ -85,16 +85,6 @@ app.use((err, req, res, next) => {
   });
 });
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.resend.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: "resend",
-    pass: process.env.RESEND_API_KEY,
-  },
-});
-
 //
 // connect to database via mongoose
 // need to fix so it doesn't connect on every request
